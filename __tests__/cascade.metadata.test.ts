40d361e6ee3452a1a29ed361eb0a73b14e5a8ef1
Michael Weber <michaelweberjr@gmail.com>
2021-07-07 11:14:29 -0400

    added a utility function to get the metadata from a cascade message

---
+++ __tests__/cascade.metadata.test.ts
@@ -0,0 +1,44 @@
+const cascade = require('../kafka-cascade/index');
+import * as Types from '../kafka-cascade/src/kafkaInterface';
+import { TestKafka } from './cascade.mockclient.test';
+
+const log = console.log;
+console.log = (test, ...args) => test === 'test' && log(args); 
+// console.log = jest.fn();
+process.env.test = 'test';
+
+describe('Cascade Metadata', () => {
+  let kafka: TestKafka;
+  let testService: any;
+
+  beforeEach(() => {
+    kafka = new TestKafka();
+  });
+
+  it('Can parse metadata', async () => {
+    const fn = cascade.getMetadata;
+    cascade.getMetadata = jest.fn(fn);
+
+    const serviceAction = (msg: Types.KafkaConsumerMessageInterface, resolve: any, reject: any) => {
+      const metadata = cascade.getMetadata(msg);
+      resolve(msg);
+    }
+    //used to ask how dlq was used
+    const dlq = jest.fn();
+
+    testService = await cascade.service(kafka, 'test-topic', 'test-group', serviceAction, jest.fn(), dlq);
+    await testService.connect();
+    await testService.run();
+
+    const producer = kafka.producer();
+    await producer.send({
+      topic: 'test-topic',
+      messages: [{
+        value: 'test message',
+      }],
+    });
+
+    expect(cascade.getMetadata).toHaveBeenCalled();
+    expect(cascade.getMetadata).toHaveReturnedWith({status:'', retries:0, topicArr:[]});
+  });
+});
