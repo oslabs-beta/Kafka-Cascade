ea7433a7375b5011d1a1808c4e5bd91a0d97188a
RCD <robert.c.du@gmail.com>
2021-07-07 11:20:44 -0400

    frontend message and topic fields
    Co-authored-by: Davette Bryan <Davette-Bryan@users.noreply.github.com>

--- demo/client/App.tsx
+++ demo/client/App.tsx
@@ -6,23 +6,49 @@ interface AppProps {
 }
 
 interface AppState {
-  
+  message: string,
+  topic: string,
 }
 
-export class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
+export const App: FC<AppProps> = () => {
+  const [message, setMessage] = useState<string>('');
+  const [topic, setTopic] = useState<string>('');
 
-    this.state = {
-      
-    };
+  const submitData = () => {
+    // fetch request to server delivering message, topic, and retry parameters
+
+    // 
+
   }
 
-  render() {
     return (
-      <div className='app'>
-        <h1>Test App</h1>
+      <div>
+        <div className='app'>
+          <h1>Test App</h1>
+        </div>
+        <div>
+          <form>
+            <label>
+              Input Message Here
+              <input
+                type="text"
+                onChange={(e) => setMessage(e.target.value)}
+              /> 
+            </label>
+            <label>
+              Input Topic Here 
+              <input
+                type = 'text'
+                onChange={(e) => setTopic(e.target.value)}
+              />
+            </label>
+          </form>
+        </div>
+        <div>
+          <button onClick={() => submitData()}>
+            Start Cascade!
+          </button>
+        </div>
       </div>
     );
-  }
 }
