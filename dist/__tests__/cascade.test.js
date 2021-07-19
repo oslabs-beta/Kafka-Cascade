790de9e9c85e53d30760f5ba8d759f85093101da
Davette Bryan <davette@Davettes-MacBook-Air.local>
2021-07-19 08:47:48 -0700

    cleaned up file tree

--- dist/__tests__/cascade.test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const { Kafka } = require('kafkajs');
-test('Empty for now', () => {
-});
--- dist/kafka-cascade/index.d.ts
+++
@@ -1,5 +0,0 @@
-import CascadeService from './src/cascadeService';
-import CascadeProducer from './src/cascadeProducer';
-import CascadeConsumer from './src/cascadeConsumer';
-import * as Types from './src/kafkaInterface';
-export { CascadeService, CascadeProducer, CascadeConsumer, Types, };
